3e2acf7330104ecbd69bc7e2288142ba04da4f59
Glockers <megamaxchanel@gmail.com>
2023-11-12 16:37:30 +0300

    feat: add logger for orders

--- apps/order/src/orders/orders.resolver.ts
+++ apps/order/src/orders/orders.resolver.ts
@@ -13,15 +13,18 @@ import { UserHelper } from '@app/common/helpers';
 import { CatalogService } from '../services/catalog.service';
 import { Basket } from '../types';
 import { JwtAuthGuard, Roles, RolesGuard } from '@app/common/auth';
-import { UseGuards } from '@nestjs/common';
+import { LoggerService, UseFilters, UseGuards } from '@nestjs/common';
 import { Role } from '@app/common/constants';
+import { HttpExceptionFilter } from '@app/common/filters';
 
 @Resolver('Order')
+@UseFilters(HttpExceptionFilter)
 export class OrdersResolver {
   constructor(
     private readonly orderService: OrderService,
     private readonly userHelper: UserHelper,
-    private readonly catalogService: CatalogService
+    private readonly catalogService: CatalogService,
+    private readonly logger: LoggerService
   ) {}
 
   @Roles()
@@ -29,7 +32,9 @@ export class OrdersResolver {
   @Mutation()
   async create_order(@Cookie(NAME_JWT_COOKIE) jwt: Tokens) {
     const userID = await this.userHelper.getUserID(jwt);
-    return await this.orderService.getStripeSession(userID);
+    const result = await this.orderService.getStripeSession(userID);
+    this.logger.log('Order created successfully', 'OrdersResolver');
+    return result;
   }
 
   @Roles(Role.Admin)
@@ -37,12 +42,25 @@ export class OrdersResolver {
   @Query()
   async orders(@Cookie(NAME_JWT_COOKIE) jwt: Tokens) {
     const userID = await this.userHelper.getUserID(jwt);
-    return await this.orderService.getOrdersByUserId(userID);
+    const result = await this.orderService.getOrdersByUserId(userID);
+    this.logger.log('Orders retrieved successfully', 'OrdersResolver');
+    return result;
   }
 
   @ResolveField('products')
   async getProducts(@Parent() basket: Basket) {
-    return await this.catalogService.getProducts(basket.productIDs);
+    try {
+      const result = await this.catalogService.getProducts(basket.productIDs);
+      this.logger.log('Products retrieved successfully', 'OrdersResolver');
+      return result;
+    } catch (error) {
+      this.logger.error(
+        `Error retrieving products: ${error.message}`,
+        error.stack,
+        'OrdersResolver'
+      );
+      throw error;
+    }
   }
   @ResolveField('user')
   async getUser(@Parent() basket: Basket) {
